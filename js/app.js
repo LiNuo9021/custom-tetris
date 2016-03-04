0abea6322f3fdfc6134e9a91937fa9d483704181
李诺 <linuo@linuodeMacBook-Air.local>
2016-03-04 11:08:47 +0800

    oo

--- js/app.js
+++ js/app.js
@@ -1,40 +1,16 @@
 Game.App = function() {
 	this._engine = null;
-	this._attacker = null;
-	this._defender = null;
 
 	this._dom = {
-		//知识点：querySelector
 		left: document.querySelector("#left"),
-		right: document.querySelector("#right"),
-		attacker: document.querySelector("#attacker"),
-		defender: document.querySelector("#defender"),
-		play: document.querySelector("#play"),
-		setup: document.querySelector("#setup")
+		right: document.querySelector("#right")
 	}
 
-	//TODO
-	this._select = {
-		attacker: this._dom.attacker.querySelector("select"),
-		defender: this._dom.defender.querySelector("select")
-	}
-
-	this._select.attacker.value = "AI";
-	this._select.defender.value = "Human";
-
-	this._dom.play.disabled = false;
-	
-	this._dom.setup.classList.add("playing");
 	setTimeout(this._start.bind(this), 500);
 }
 
 
 Game.App.prototype._start = function() {
-	this._dom.play.removeEventListener("click", this);
-
-	this._dom.left.appendChild(this._dom.defender);
-	this._dom.right.appendChild(this._dom.attacker);
-	
 	this._engine = new Game.Engine();
 
 	new Game.Defender["Human"](this._engine);
